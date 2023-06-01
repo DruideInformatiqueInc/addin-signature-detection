e1c6245c41beff0383b7c0ff8ffcf490189f06f4
Simon Langevin <langevin.simon@gmail.com>
2023-06-01 16:04:26 -0400

    Ajoute l'interception

--- src/events.js
+++ src/events.js
@@ -4,9 +4,9 @@ Office.initialize = () => { }
 const setSignature = (eventType, event) => {
     Office.context.mailbox.item.body.setSignatureAsync(eventType, { coercionType : 'html' }, (asyncResult)=> {
         if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
-            console.error(result.error.message)
+            console.error(asyncResult.error.message)
         }
-        console.log(result.value)
+        console.log(asyncResult.value)
         event.completed()
     })
 }
@@ -16,4 +16,8 @@ Office.actions.associate('OnNewMessageCompose', (event) => {
 })
 Office.actions.associate('OnMessageRecipientsChanged', (event) => {
     setSignature('OnMessageRecipientsChanged', event)
+})
+
+Office.actions.associate('onMessageSendHandler', (event) => {
+    event.completed({ allowEvent: false})
 })
